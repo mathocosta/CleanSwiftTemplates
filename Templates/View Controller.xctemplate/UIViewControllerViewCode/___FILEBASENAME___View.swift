7c02769ed36b59a342bd938eb530429ac1fec2e5
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2020-08-11 17:50:41 -0300

    add options for user interface on view controller template

---
+++ Templates/View Controller.xctemplate/UIViewControllerViewCode/___FILEBASENAME___View.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILEHEADER___
+//
+
+import UIKit
+
+class ___VARIABLE_sceneName___View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
